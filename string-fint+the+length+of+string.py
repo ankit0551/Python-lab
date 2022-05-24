74dee048744aa79b2c9cda6972d5be1d07756d23
Ankit Kumar <92089461+ankit0551@users.noreply.github.com>
2022-05-24 23:10:00 +0530

    Add files via upload

---
+++ string-fint+the+length+of+string.py
@@ -0,0 +1,8 @@
+st = input("Enter the string ")
+print(len(st))
+
+#*********************************************************************************************#
+count = 0
+for i in st:
+    count += 1
+print(count)
