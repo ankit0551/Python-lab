c4544326a503ca44d38556cff0dde6ecfad5e72d
Ankit Kumar <92089461+ankit0551@users.noreply.github.com>
2022-05-30 01:06:52 +0530

    Add files via upload

---
+++ pattern-dimond.py
@@ -0,0 +1,13 @@
+a = int(input())
+for i in range(a):
+    for j in range(a-1-i):
+        print(' ',end='')
+    for j in range(2*i+1):
+        print('*',end='')
+    print()
+for i in range(1,a):
+    for j in range(i):
+        print(' ',end='')
+    for j in range(2*(a-i)-1):
+        print('*',end='')
+    print()
